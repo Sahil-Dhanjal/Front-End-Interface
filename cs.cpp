421c165f8ee01a5001e2e9056d0836bb9477de03
Sahil Dhanjal <53312852+Sahil-Dhanjal@users.noreply.github.com>
2022-05-20 14:42:53 +0530

    Update and rename cs.py to cs.cpp

---
+++ cs.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+ 
+string encrypt(string text, int s)
+{
+    string result = "";
+ 
+    for (int i=0;i<text.length();i++)
+    {
+        if (isupper(text[i]))
+        result += char(int(text[i]+s-65)%26 +65);
+ 
+    	else
+        result += char(int(text[i]+s-97)%26 +97);
+    }
+ 
+    return result;
+}
+
+string decrypt(string text, int s)
+{
+	string result = "";
+	
+	for (int i=0;i<text.length();i++)
+	{
+		if (isupper(text[i]))
+		result += char(int(text[i]-s+65)%26 +65);
+		
+		else
+		result += char(int(text[i]-s+97)%26 +97);
+	}
+	
+	return result;
+}
+
+
+int main()
+{
+    string text="ABC";
+    int s = 3;
+    cout << "Text : " << text;
+    cout << "\nShift: " << s;
+    cout << "\nCipher: " << encrypt(text, s);
+    cout << "\nCipher: " << decrypt(text, s);
+    return 0;
+}
